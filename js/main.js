511b579b24c69941d592213d33a96cb3b6d3787b
Lulu <mandydl33@gmail.com>
2016-12-27 20:59:10 -0800

    added font awesome icons to page

--- js/main.js
+++ js/main.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
   $('section#screenshots a').on('click', function() {
     $('div#modal img').attr('src', $(this).attr('data-image-url') );
   });
+
+  var nav = $('.navbar-fixed-top');
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 300) {
+      nav.addClass('effect');
+    } // end if
+    else {
+      nav.removeClass('effect');
+    } // end else
+  }); // end .scroll
 });
